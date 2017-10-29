0fa8c6c65391c81b7bb657bf5071e6018b82dc5c
Andreas Evers <krosan@gmail.com>
2017-10-30 00:23:57 +0100

    Fixed stupid errors

--- src/main/java/com/spinnakerdemo/app/SpinnakerDemoApplication.java
+++ src/main/java/com/spinnakerdemo/app/SpinnakerDemoApplication.java
@@ -1,4 +1,4 @@
-package com.spinnakerdemo.spinnakerdemo;
+package com.spinnakerdemo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/spinnakerdemo/app/SpinnakerDemoApplicationTests.java
+++ src/test/java/com/spinnakerdemo/app/SpinnakerDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spinnakerdemo.spinnakerdemo;
+package com.spinnakerdemo.app;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
