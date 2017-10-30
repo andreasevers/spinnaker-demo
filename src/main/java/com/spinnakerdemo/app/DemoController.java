f24d233f592acc502e7485c3336c9d1b2ca3eeaf
Andreas Evers <krosan@gmail.com>
2017-10-31 00:26:44 +0100

    Fixing caching issue

--- src/main/java/com/spinnakerdemo/app/DemoController.java
+++ src/main/java/com/spinnakerdemo/app/DemoController.java
@@ -3,13 +3,10 @@ package com.spinnakerdemo.app;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * @author Andreas Evers
- */
 @Controller
 public class DemoController {
 
-    @RequestMapping("/demo")
+    @RequestMapping("/")
     public String getView() throws InterruptedException {
 //        Thread.sleep(1000);
         return "demo";
--- src/main/java/com/spinnakerdemo/app/WebConfig.java
+++ src/main/java/com/spinnakerdemo/app/WebConfig.java
@@ -1,12 +1,10 @@
 package com.spinnakerdemo.app;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
    @Override
