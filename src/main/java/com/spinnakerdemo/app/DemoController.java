1f2a18bbcb981c8104e9b864d539799819e74019
Andreas Evers <krosan@gmail.com>
2017-11-01 11:00:43 +0100

    Disabled caching

--- src/main/java/com/spinnakerdemo/app/DemoController.java
+++ src/main/java/com/spinnakerdemo/app/DemoController.java
@@ -1,13 +1,21 @@
 package com.spinnakerdemo.app;
 
+import org.springframework.http.CacheControl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class DemoController {
 
     @RequestMapping("/")
-    public String getView() throws InterruptedException {
+    public String getView(HttpServletResponse response) throws InterruptedException {
+        String headerValue = CacheControl.maxAge(0, TimeUnit.SECONDS)
+                .getHeaderValue();
+
+        response.addHeader("Cache-Control", headerValue);
 //        Thread.sleep(1000);
         return "demo";
     }
