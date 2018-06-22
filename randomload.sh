88c6639613c0ffd6893435324bfdb7ef88db8504
Andreas Evers <krosan@gmail.com>
2018-06-22 12:47:17 +0200

    Switched to PickleRick + decreased delay in load script

--- randomload.sh
+++ randomload.sh
@@ -5,7 +5,7 @@ number=$((RANDOM % range))
 while true
 do
    http 35.233.120.240 | grep background
-   echo "Sleeping $number seconds"
-   sleep $number
+   echo "Sleeping 0.$number seconds"
+   sleep 0.$number
    number=$((RANDOM % range))
 done
--- src/main/java/com/spinnakerdemo/app/DemoController.java
+++ src/main/java/com/spinnakerdemo/app/DemoController.java
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @Controller
 public class DemoController {
@@ -16,12 +17,12 @@ public class DemoController {
 
     @RequestMapping("/")
     public String getView(HttpServletResponse response) {
-        String headerValue = CacheControl.maxAge(0, TimeUnit.SECONDS)
+        String headerValue = CacheControl.maxAge(0, SECONDS)
                 .getHeaderValue();
 
         response.addHeader("Cache-Control", headerValue);
         response.addHeader("Connection", "close");
-        logger.info("Meeseeks");
+//        logger.info("Meeseeks");
         return "demo";
     }
 }
