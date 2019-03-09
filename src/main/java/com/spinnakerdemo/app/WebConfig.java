c09d5077b9537655e6b5b26f20058566595cf9c4
Andreas Evers <andreas.evers@rabobank.nl>
2019-03-09 12:00:51 +0100

    Fixed surefire ForkedBooter issue

--- src/main/java/com/spinnakerdemo/app/WebConfig.java
+++ src/main/java/com/spinnakerdemo/app/WebConfig.java
@@ -1,5 +1,8 @@
 package com.spinnakerdemo.app;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -11,6 +14,18 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").setCachePeriod(0);
+    }
+
+    @Bean
+    public GracefulShutdown gracefulShutdown() {
+        return new GracefulShutdown();
+    }
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory(final GracefulShutdown gracefulShutdown) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(gracefulShutdown);
+        return factory;
     }
 
 }
