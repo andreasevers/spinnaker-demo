c898828b670668b487edc288c21341d79d055c66
Andreas Evers <andreas.evers@rabobank.nl>
2019-03-09 14:23:03 +0100

    Switched to picklerick

--- src/main/java/com/spinnakerdemo/app/DemoController.java
+++ src/main/java/com/spinnakerdemo/app/DemoController.java
@@ -22,7 +22,7 @@ public class DemoController {
 
         response.addHeader("Cache-Control", headerValue);
         response.addHeader("Connection", "close");
-        logger.info("Meeseeks");
+        //logger.info("Meeseeks");
         return "demo";
     }
 
