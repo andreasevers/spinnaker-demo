b507de330d40e8b54f74be77192c19aad837e5ad
Andreas Evers <krosan@gmail.com>
2019-09-25 17:29:25 +0200

    Switched to Deployments + changed ip address in randomload

--- randomload.sh
+++ randomload.sh
@@ -4,7 +4,7 @@ number=$((RANDOM % range))
 
 while true
 do
-   http 35.245.157.54 | grep background
+   http 35.245.16.174 | grep background
    echo "Sleeping 0.$number seconds"
    sleep 0.$number
    number=$((RANDOM % range))
